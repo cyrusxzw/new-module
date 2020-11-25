b82341c24a7be62256c786fd339aa8361d73896e
Beau Charman <beau.charman@gmail.com>
2020-11-25 14:27:57 +1100

    feat: aes-2264 add testing library dependancies and replace enzyme in breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.spec.js
+++ src/components/Breadcrumbs/Breadcrumbs.spec.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import BreadcrumbsFixture from './Breadcrumbs.fixture';
 import Breadcrumbs from './Breadcrumbs';
-
-configure({ adapter: new Adapter() });
 
 describe('<Breadcrumbs />', () => {
   it('should be defined', () => {
@@ -14,14 +11,23 @@ describe('<Breadcrumbs />', () => {
 
   it('renders base component correctly', () => {
     const tree = renderer
-      .create(
-        <Breadcrumbs
-          items={BreadcrumbsFixture.items}
-          theme={BreadcrumbsFixture.theme}
-        />,
-      )
+      .create(<Breadcrumbs items={BreadcrumbsFixture.items} />)
       .toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should return `null` if the length of the `items` prop is 0', () => {
+    const { container } = render(<Breadcrumbs items={[]} />);
+    expect(container.firstChild).toBeNull();
+  });
+
+  it('should render the corrct number of items', async () => {
+    const { getAllByRole } = render(
+      <Breadcrumbs items={BreadcrumbsFixture.items} />,
+    );
+    const listItems = getAllByRole('listitem');
+
+    expect(listItems).toHaveLength(2);
   });
 });
