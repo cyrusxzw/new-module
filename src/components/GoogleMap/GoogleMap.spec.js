ee3be8570a10b3f0f1e01c622988b4a0002d3fa8
Beau Charman <beau.charman@gmail.com>
2020-12-03 13:37:45 +1100

    feat: refactor google maps test

--- src/components/GoogleMap/GoogleMap.spec.js
+++ src/components/GoogleMap/GoogleMap.spec.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { GoogleMapsContextProvider } from '~/contexts/GoogleMaps.context';
-import constants from '~.storybook/constants';
 import GoogleMap from './GoogleMap';
 import GoogleMapFixture from './GoogleMap.fixture';
 
@@ -12,19 +10,15 @@ describe('<GoogleMap />', () => {
 
   it('renders base component correctly', () => {
     const { container } = render(
-      <GoogleMapsContextProvider
-        apiKey={constants.GOOGLE_MAPS.DEVELOPMENT_API_KEY}
-      >
-        <GoogleMap
-          center={GoogleMapFixture.customPinMarkerCenter}
-          copy={GoogleMapFixture.copy}
-          customMarker={GoogleMapFixture.customPinMarker}
-          hasMarkerIndexes={false}
-          id="google-map"
-          initialZoom={GoogleMapFixture.initialZoom}
-          places={GoogleMapFixture.places}
-        />
-      </GoogleMapsContextProvider>,
+      <GoogleMap
+        center={GoogleMapFixture.customPinMarkerCenter}
+        copy={GoogleMapFixture.copy}
+        customMarker={GoogleMapFixture.customPinMarker}
+        hasMarkerIndexes={false}
+        id="google-map"
+        initialZoom={GoogleMapFixture.initialZoom}
+        places={GoogleMapFixture.places}
+      />,
     );
 
     expect(container).toMatchSnapshot();
