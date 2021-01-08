763729d5297d50297e0816dac8b54cdccf5f669c
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2021-01-08 15:51:00 +1100

    feat(checkbox): forward ref the component

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,53 +1,59 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { v4 as uuidv4 } from 'uuid';
 import styles from './Checkbox.module.css';
 
-const Checkbox = ({
-  className,
-  checkboxClassName,
-  content,
-  contentClassName,
-  dataTestRef,
-  errorMessage,
-  id: idProp,
-  isEnabled,
-  name,
-  onChange,
-  theme,
-}) => {
-  const baseClassSet = cx(styles.base, className);
-  const checkboxClassSet = cx(
-    styles.checkBox,
-    styles[theme],
-    { [styles.hasError]: errorMessage },
-    checkboxClassName,
-  );
-  const contentClassSet = cx(styles.content, styles[theme], contentClassName);
+const Checkbox = forwardRef(
+  (
+    {
+      className,
+      checkboxClassName,
+      content,
+      contentClassName,
+      dataTestRef,
+      errorMessage,
+      id: idProp,
+      isEnabled,
+      name,
+      onChange,
+      theme,
+    },
+    ref,
+  ) => {
+    const baseClassSet = cx(styles.base, className);
+    const checkboxClassSet = cx(
+      styles.checkBox,
+      styles[theme],
+      { [styles.hasError]: errorMessage },
+      checkboxClassName,
+    );
+    const contentClassSet = cx(styles.content, styles[theme], contentClassName);
 
-  const id = idProp || uuidv4();
+    const id = idProp || uuidv4();
 
-  return (
-    <React.Fragment>
-      <label className={baseClassSet} htmlFor={id}>
-        <input
-          className={checkboxClassSet}
-          data-test-ref={dataTestRef}
-          disabled={!isEnabled}
-          id={id}
-          name={name}
-          onChange={onChange}
-          type="checkbox"
-        />
-        <span className={contentClassSet}>{content}</span>
-      </label>
-      {errorMessage && (
-        <span className={cx(styles.errorMessage)}>{errorMessage}</span>
-      )}
-    </React.Fragment>
-  );
-};
+    return (
+      <React.Fragment>
+        <label className={baseClassSet} htmlFor={id}>
+          <input
+            className={checkboxClassSet}
+            data-test-ref={dataTestRef}
+            disabled={!isEnabled}
+            id={id}
+            name={name}
+            onChange={onChange}
+            ref={ref}
+            type="checkbox"
+          />
+          <span className={contentClassSet}>{content}</span>
+        </label>
+        {errorMessage && (
+          <span className={cx(styles.errorMessage)}>{errorMessage}</span>
+        )}
+      </React.Fragment>
+    );
+  },
+);
 
 Checkbox.propTypes = {
   className: PropTypes.string,
