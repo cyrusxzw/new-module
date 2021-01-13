db001e0aa8c2a30fa12893e0a140a7cd98eff764
Beau Charman <beau.charman@gmail.com>
2021-01-14 10:30:20 +1100

    feat: aes-684 export named defaults to be exposed at the top level

--- src/index.js
+++ src/index.js
@@ -1,9 +1,98 @@
-import components from './components';
-import compositions from './compositions';
+import components, {
+  Accordion,
+  AddToCartButton,
+  Audio,
+  BodyCopy,
+  Breadcrumbs,
+  Button,
+  BynderWidget,
+  Carousel,
+  Checkbox,
+  ConditionalWrapper,
+  ContentHubArticle,
+  ContentHubArticleList,
+  DefinitionList,
+  DialogBanner,
+  DoubleMedia,
+  Figure,
+  FlyinPanel,
+  FooterBlock,
+  TextInputV2,
+  FullWidthHeroScroll,
+  GoogleMap,
+  Heading,
+  Hidden,
+  HorizontalProductDisplayAccordion,
+  Hyperlink,
+  Icon,
+  IconLink,
+  Image,
+  ImageCarousel,
+  KitList,
+  LinkButtonGroup,
+  List,
+  LoadMoreButton,
+  Loading,
+  MediaWithContent,
+  Modal,
+  NavigationBar,
+  NotificationModal,
+  Overlay,
+  P,
+  Paragraph,
+  ParagraphSet,
+  Podium,
+  ProductCommerce,
+  ProductExtract,
+  ProductGridItem,
+  ProductDetailHeader,
+  Quote,
+  RadioGroup,
+  ReadMore,
+  SecondaryMessage,
+  SectionHeading,
+  Select,
+  StoreDetailHeader,
+  StoreHoursList,
+  SubNav,
+  Textarea,
+  TextInput,
+  TextOverFullWidthAsset,
+  Transition,
+  TwoColumnLayout,
+  TwoColumnList,
+  Video,
+} from './components';
+import compositions, { Footer, NewsletterSignUp } from './compositions';
 import constants from './constants';
-import contexts from './contexts';
-import customHooks from './customHooks';
-import utils from './utils';
+import contexts, {
+  AddToCartContextProvider,
+  useAddToCartContext,
+  GoogleMapsContextProvider,
+  useGoogleMapsContext,
+  LoadMoreContextProvider,
+  useLoadMoreContext,
+  NavBarThemeContextProvider,
+  useNavBarThemeContext,
+  NotificationContextProvider,
+  useNotificationContext,
+  ProductDetailContextProvider,
+  useProductDetailContext,
+  ThemeContextProvider,
+  useThemeContext,
+  VariantSelectContextProvider,
+  useVariantSelectContext,
+} from './contexts';
+import customHooks, {
+  useEscapeKeyListener,
+  useHasMounted,
+  useImageTransition,
+  useOnScreen,
+  useOverflowHidden,
+  useScript,
+  useWindowHasResized,
+} from './customHooks';
+import utils, { environment, objects, product, viewports } from './utils';
 
 /* eslint-disable-next-line import/no-default-export */
 export default {
@@ -14,3 +103,113 @@ export default {
   customHooks,
   utils,
 };
+
+/** components */
+export { components };
+export { Accordion };
+export { AddToCartButton };
+export { Audio };
+export { BodyCopy };
+export { Breadcrumbs };
+export { Button };
+export { BynderWidget };
+export { Carousel };
+export { Checkbox };
+export { ConditionalWrapper };
+export { ContentHubArticle };
+export { ContentHubArticleList };
+export { DefinitionList };
+export { DialogBanner };
+export { DoubleMedia };
+export { Figure };
+export { FlyinPanel };
+export { FooterBlock };
+export { TextInputV2 };
+export { FullWidthHeroScroll };
+export { GoogleMap };
+export { Heading };
+export { Hidden };
+export { HorizontalProductDisplayAccordion };
+export { Hyperlink };
+export { Icon };
+export { IconLink };
+export { Image };
+export { ImageCarousel };
+export { KitList };
+export { LinkButtonGroup };
+export { List };
+export { LoadMoreButton };
+export { Loading };
+export { MediaWithContent };
+export { Modal };
+export { NavigationBar };
+export { NotificationModal };
+export { Overlay };
+export { P };
+export { Paragraph };
+export { ParagraphSet };
+export { Podium };
+export { ProductCommerce };
+export { ProductExtract };
+export { ProductGridItem };
+export { ProductDetailHeader };
+export { Quote };
+export { RadioGroup };
+export { ReadMore };
+export { SecondaryMessage };
+export { SectionHeading };
+export { Select };
+export { StoreDetailHeader };
+export { StoreHoursList };
+export { SubNav };
+export { Textarea };
+export { TextInput };
+export { TextOverFullWidthAsset };
+export { Transition };
+export { TwoColumnLayout };
+export { TwoColumnList };
+export { Video };
+
+/** compositions */
+export { compositions };
+export { Footer };
+export { NewsletterSignUp };
+
+/** constants */
+export { constants };
+
+/** contexts */
+export { contexts };
+export { AddToCartContextProvider };
+export { useAddToCartContext };
+export { GoogleMapsContextProvider };
+export { useGoogleMapsContext };
+export { LoadMoreContextProvider };
+export { useLoadMoreContext };
+export { NavBarThemeContextProvider };
+export { useNavBarThemeContext };
+export { NotificationContextProvider };
+export { useNotificationContext };
+export { ProductDetailContextProvider };
+export { useProductDetailContext };
+export { ThemeContextProvider };
+export { useThemeContext };
+export { VariantSelectContextProvider };
+export { useVariantSelectContext };
+
+/** customHooks */
+export { customHooks };
+export { useEscapeKeyListener };
+export { useHasMounted };
+export { useImageTransition };
+export { useOnScreen };
+export { useOverflowHidden };
+export { useScript };
+export { useWindowHasResized };
+
+/** utils */
+export { utils };
+export { environment };
+export { objects };
+export { product };
+export { viewports };
