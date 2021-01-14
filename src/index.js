536fb4b07e80641dfd752912eded674ac38b94bb
Beau Charman <beau.charman@gmail.com>
2021-01-14 12:54:41 +1100

    feat: aes-684 force named exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import components, {
+export * as components from './components';
+export {
   Accordion,
   AddToCartButton,
   Audio,
@@ -63,9 +64,14 @@ import components, {
   TwoColumnList,
   Video,
 } from './components';
-import compositions, { Footer, NewsletterSignUp } from './compositions';
-import constants from './constants';
-import contexts, {
+
+export * as compositions from './compositions';
+export { Footer, NewsletterSignUp } from './compositions';
+
+export * as constants from './constants';
+
+export * as contexts from './contexts';
+export {
   AddToCartContextProvider,
   useAddToCartContext,
   GoogleMapsContextProvider,
@@ -83,7 +89,9 @@ import contexts, {
   VariantSelectContextProvider,
   useVariantSelectContext,
 } from './contexts';
-import customHooks, {
+
+export * as customHooks from './customHooks';
+export {
   useEscapeKeyListener,
   useHasMounted,
   useImageTransition,
@@ -92,124 +100,6 @@ import customHooks, {
   useScript,
   useWindowHasResized,
 } from './customHooks';
-import utils, { environment, objects, product, viewports } from './utils';
 
-/* eslint-disable-next-line import/no-default-export */
-export default {
-  components,
-  compositions,
-  constants,
-  contexts,
-  customHooks,
-  utils,
-};
-
-/** components */
-export { components };
-export { Accordion };
-export { AddToCartButton };
-export { Audio };
-export { BodyCopy };
-export { Breadcrumbs };
-export { Button };
-export { BynderWidget };
-export { Carousel };
-export { Checkbox };
-export { ConditionalWrapper };
-export { ContentHubArticle };
-export { ContentHubArticleList };
-export { DefinitionList };
-export { DialogBanner };
-export { DoubleMedia };
-export { Figure };
-export { FlyinPanel };
-export { FooterBlock };
-export { TextInputV2 };
-export { FullWidthHeroScroll };
-export { GoogleMap };
-export { Heading };
-export { Hidden };
-export { HorizontalProductDisplayAccordion };
-export { Hyperlink };
-export { Icon };
-export { IconLink };
-export { Image };
-export { ImageCarousel };
-export { KitList };
-export { LinkButtonGroup };
-export { List };
-export { LoadMoreButton };
-export { Loading };
-export { MediaWithContent };
-export { Modal };
-export { NavigationBar };
-export { NotificationModal };
-export { Overlay };
-export { P };
-export { Paragraph };
-export { ParagraphSet };
-export { Podium };
-export { ProductCommerce };
-export { ProductExtract };
-export { ProductGridItem };
-export { ProductDetailHeader };
-export { Quote };
-export { RadioGroup };
-export { ReadMore };
-export { SecondaryMessage };
-export { SectionHeading };
-export { Select };
-export { StoreDetailHeader };
-export { StoreHoursList };
-export { SubNav };
-export { Textarea };
-export { TextInput };
-export { TextOverFullWidthAsset };
-export { Transition };
-export { TwoColumnLayout };
-export { TwoColumnList };
-export { Video };
-
-/** compositions */
-export { compositions };
-export { Footer };
-export { NewsletterSignUp };
-
-/** constants */
-export { constants };
-
-/** contexts */
-export { contexts };
-export { AddToCartContextProvider };
-export { useAddToCartContext };
-export { GoogleMapsContextProvider };
-export { useGoogleMapsContext };
-export { LoadMoreContextProvider };
-export { useLoadMoreContext };
-export { NavBarThemeContextProvider };
-export { useNavBarThemeContext };
-export { NotificationContextProvider };
-export { useNotificationContext };
-export { ProductDetailContextProvider };
-export { useProductDetailContext };
-export { ThemeContextProvider };
-export { useThemeContext };
-export { VariantSelectContextProvider };
-export { useVariantSelectContext };
-
-/** customHooks */
-export { customHooks };
-export { useEscapeKeyListener };
-export { useHasMounted };
-export { useImageTransition };
-export { useOnScreen };
-export { useOverflowHidden };
-export { useScript };
-export { useWindowHasResized };
-
-/** utils */
-export { utils };
-export { environment };
-export { objects };
-export { product };
-export { viewports };
+export * as utils from './utils';
+export { environment, objects, product, viewports } from './utils';
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,17 +1,4 @@
-import * as environment from './environment';
-import * as objects from './objects';
-import * as product from './product';
-import * as viewports from './viewports';
-
-export { environment };
-export { objects };
-export { product };
-export { viewports };
-
-/* eslint-disable-next-line import/no-default-export */
-export default {
-  environment,
-  objects,
-  product,
-  viewports,
-};
+export * as environment from './environment';
+export * as objects from './objects';
+export * as product from './product';
+export * as viewports from './viewports';
