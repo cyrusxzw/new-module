39f151d03611bd998534fdd2c372c06e03a0401b
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2021-02-25 08:57:43 +1100

    fix(dynamicform): use IE friendly version
    Re-enable component

--- src/compositions/DynamicForm/wrappers/Select.js
+++ src/compositions/DynamicForm/wrappers/Select.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Controller } from 'react-hook-form';
+import { Controller } from 'react-hook-form/dist/index.ie11';
 import { Select } from '~/components/Select';
 
 export const SelectWrapper = ({
