3edb424957840329d3c056ac62aefbd0ac7b0720
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2021-03-02 15:24:29 +1100

    fix: wrap document access in window check

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,15 +6,19 @@ import { useEscapeKeyListener, useOverflowHidden } from '~/customHooks';
 import { ModalBody } from './components/ModalBody';
 import { Overlay } from '~/components/Overlay';
 import { Transition } from '~/components/Transition';
+import { isInBrowser } from '~/utils/environment';
 import styles from './Modal.module.css';
 
-/** Set up the Modal component's anchor point for ReactDOM.createPortal */
-let modalRoot = document.getElementById('aesop-gel-modal-root');
+let modalRoot = null;
+if (isInBrowser()) {
+  /** Set up the Modal component's anchor point for ReactDOM.createPortal */
+  modalRoot = document.getElementById('aesop-gel-modal-root');
 
-if (!modalRoot) {
-  modalRoot = document.createElement('div');
-  modalRoot.setAttribute('id', 'aesop-gel-modal-root');
-  document.body.appendChild(modalRoot);
+  if (!modalRoot) {
+    modalRoot = document.createElement('div');
+    modalRoot.setAttribute('id', 'aesop-gel-modal-root');
+    document.body.appendChild(modalRoot);
+  }
 }
 
 const Modal = ({ children, className, copy, isVisible, onClose, theme }) => {
