1941f31b12eaffb8c047e18c0d556088cc0d5253
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2021-03-25 10:11:43 +1100

    feat: migrate BynderWidget to TS

---
+++ src/components/BynderWidget/BynderWidget.types.ts
@@ -0,0 +1,10 @@
+import type { Themes } from '~/types';
+
+type BynderWidgetProps = {
+  className?: string;
+  heading: string;
+  id: string;
+  theme?: Themes;
+};
+
+export type { BynderWidgetProps };
