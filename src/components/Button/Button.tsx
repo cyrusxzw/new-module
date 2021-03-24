adede1e20d025c9e9e8284315be33819c16bb792
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2021-03-24 13:17:53 +1100

    feat: migrate Button to TS

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,51 @@
+import React, { forwardRef } from 'react';
+import cx from 'classnames';
+import type { ButtonProps } from './Button.types';
+import styles from './Button.module.css';
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(function ButtonRef(
+  {
+    children,
+    className,
+    dataTestRef,
+    id,
+    isAlternate = false,
+    isEnabled = true,
+    isInline = false,
+    onClick,
+    tabIndex,
+    title,
+    type = 'button',
+    theme = 'dark',
+  },
+  ref,
+) {
+  if (!children) return null;
+
+  const classSet = cx(
+    styles.base,
+    { [styles.alternate]: isAlternate },
+    { [styles.blockStyle]: !isInline },
+    styles[theme],
+    { [styles.disabled]: !isEnabled },
+    className,
+  );
+
+  return (
+    <button
+      className={classSet}
+      data-test-ref={dataTestRef}
+      disabled={!isEnabled}
+      id={id}
+      onClick={onClick}
+      ref={ref}
+      tabIndex={tabIndex}
+      title={title}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+});
+
+export { Button };
