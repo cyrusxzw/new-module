6a3dc194d2f15b1859df50a0661ba56add8ef0fd
Beau Charman <beau.charman@gmail.com>
2021-04-08 13:34:32 +1000

    feat: create portal root util to share logic

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,24 +3,12 @@ import { createPortal } from 'react-dom';
 import cx from 'classnames';
 import { useThemeContext } from '~/contexts';
 import { useEscapeKeyListener, useOverflowHidden } from '~/customHooks';
-import { isInBrowser } from '~/utils/environment';
+import { getPortalRoot } from '~/utils/portal';
 import { ModalBody } from './components/ModalBody';
 import { Overlay } from '~/components/Overlay';
 import { Transition } from '~/components/Transition';
 import type { ModalProps } from './Modal.types';
 import styles from './Modal.module.css';
-
-let modalRoot = null;
-if (isInBrowser()) {
-  /** Set up the Modal component's anchor point for ReactDOM.createPortal */
-  modalRoot = document.getElementById('aesop-gel-modal-root');
-
-  if (!modalRoot) {
-    modalRoot = document.createElement('div');
-    modalRoot.setAttribute('id', 'aesop-gel-modal-root');
-    document.body.appendChild(modalRoot);
-  }
-}
 
 const Modal: FC<ModalProps> = ({
   children,
@@ -67,7 +55,7 @@ const Modal: FC<ModalProps> = ({
             </aside>
           </Transition>
         </>,
-        modalRoot,
+        getPortalRoot('aesop-gel-modal-root'),
       )}
     </>
   );
