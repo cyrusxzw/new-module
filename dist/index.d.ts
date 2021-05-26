ad341183e53adf15d806f5f676e209b59424a4f1
Beau Charman <beau.charman@gmail.com>
2021-05-26 15:17:05 +1000

    feat(con-383): migrate two column layout to typescript

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1511,7 +1511,7 @@ declare type SectionHeadingProps = {
     titleFont?: 'Suisse' | 'Zapf';
 };
 
-declare const SectionHeading: ({ childrenClassNames, className, eyebrow, hasSerifFontHeading, heading, id, isFlush, isOffsetPageHeading, isHeroHeading, isPageHeading, subHeading, theme, titleFont, }: SectionHeadingProps) => ReactElement;
+declare const SectionHeading: ({ childrenClassNames, className, eyebrow, hasSerifFontHeading, heading, id, isFlush, isOffsetPageHeading, isHeroHeading, isPageHeading, subHeading, theme, titleFont, }: SectionHeadingProps) => ReactElement | null;
 
 declare const Select: React$1.ForwardRefExoticComponent<React$1.RefAttributes<any>>;
 
@@ -1785,58 +1785,22 @@ declare namespace TextOverFullWidthAsset {
 
 declare const Transition: ({ children, isActive, isActiveOnMount, shouldMountOnEnter, shouldUnmountOnExit, type, }: TransitionProps) => ReactElement | null;
 
-declare function TwoColumnLayout({ childrenClassNames, className, content, hasFullWidthContent, id, isFlushOnSmall, isReversed, sidebar, theme, }: {
-    childrenClassNames: any;
-    className: any;
-    content: any;
-    hasFullWidthContent: any;
-    id: any;
-    isFlushOnSmall: any;
-    isReversed: any;
-    sidebar: any;
-    theme: any;
-}): JSX.Element;
-declare namespace TwoColumnLayout {
-    namespace propTypes {
-        const childrenClassNames: PropTypes.Requireable<PropTypes.InferProps<{
-            sidebar: PropTypes.Requireable<string>;
-            content: PropTypes.Requireable<string>;
-        }>>;
-        const className: PropTypes.Requireable<string>;
-        const content: PropTypes.Validator<any>;
-        const hasFullWidthContent: PropTypes.Requireable<boolean>;
-        const id: PropTypes.Requireable<string>;
-        const isFlushOnSmall: PropTypes.Requireable<boolean>;
-        const isReversed: PropTypes.Requireable<boolean>;
-        const sidebar: PropTypes.Requireable<any>;
-        const theme: PropTypes.Requireable<string>;
-    }
-    namespace defaultProps {
-        export namespace childrenClassNames_1 {
-            const sidebar_1: any;
-            export { sidebar_1 as sidebar };
-            const content_1: any;
-            export { content_1 as content };
-        }
-        export { childrenClassNames_1 as childrenClassNames };
-        const className_1: any;
-        export { className_1 as className };
-        const content_2: any;
-        export { content_2 as content };
-        const hasFullWidthContent_1: boolean;
-        export { hasFullWidthContent_1 as hasFullWidthContent };
-        const id_1: any;
-        export { id_1 as id };
-        const isFlushOnSmall_1: boolean;
-        export { isFlushOnSmall_1 as isFlushOnSmall };
-        const isReversed_1: boolean;
-        export { isReversed_1 as isReversed };
-        const sidebar_2: any;
-        export { sidebar_2 as sidebar };
-        const theme_1: string;
-        export { theme_1 as theme };
-    }
-}
+declare type TwoColumnLayoutProps = {
+    childrenClassNames?: {
+        sidebar?: string;
+        content?: string;
+    };
+    className?: string;
+    content: React.ReactNode;
+    hasFullWidthContent?: boolean;
+    id?: string;
+    isFlushOnSmall?: boolean;
+    isReversed?: boolean;
+    sidebar?: React.ReactNode;
+    theme?: Themes;
+};
+
+declare const TwoColumnLayout: ({ childrenClassNames, className, content, hasFullWidthContent, id, isFlushOnSmall, isReversed, sidebar, theme, }: TwoColumnLayoutProps) => ReactElement;
 
 /**
  * Not using HyperlinkType as url is not required here
---
+++ src/components/TwoColumnLayout/TwoColumnLayout.tsx
@@ -0,0 +1,55 @@
+import React, { ReactElement } from 'react';
+import { useUID } from 'react-uid';
+import cx from 'classnames';
+import { useThemeContext } from '~/contexts';
+import type { TwoColumnLayoutProps } from './TwoColumnLayout.types';
+import styles from './TwoColumnLayout.module.css';
+
+const TwoColumnLayout = ({
+  childrenClassNames,
+  className,
+  content,
+  hasFullWidthContent = false,
+  id,
+  isFlushOnSmall = false,
+  isReversed = true,
+  sidebar,
+  theme,
+}: TwoColumnLayoutProps): ReactElement => {
+  const currentTheme = useThemeContext(theme, 'dark');
+  const uid = useUID();
+
+  const classSet = cx(
+    styles.base,
+    { [styles.reverse]: isReversed },
+    { [styles.contentOnly]: !sidebar },
+    styles[currentTheme],
+    className,
+  );
+  const contentClassSet = cx(
+    styles.content,
+    { [styles.fullWidth]: hasFullWidthContent },
+    { [styles.flushOnSmall]: isFlushOnSmall },
+    childrenClassNames?.content,
+  );
+  const sidebarClassSet = cx(styles.sidebar, childrenClassNames?.sidebar);
+
+  return (
+    <div className={classSet} id={id}>
+      <div
+        className={contentClassSet}
+        data-testid="data-testid-TwoColumnLayout"
+        id={`${id || uid}-mainColumn`}
+      >
+        {content}
+      </div>
+      {sidebar && (
+        <aside className={sidebarClassSet} id={`${id || uid}-sidebarColumn`}>
+          {sidebar}
+        </aside>
+      )}
+    </div>
+  );
+};
+
+export { TwoColumnLayout };
