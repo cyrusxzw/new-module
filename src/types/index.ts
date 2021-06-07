3ca2bd148e3345bbc4fbfbb46d1af4cd39700e9c
Beau Charman <beau.charman@gmail.com>
2021-06-07 13:36:13 +1000

    chore: type exports

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export type { Product } from './Product.types';
 export type { Themes } from './Themes.types';
 export type { Variant } from './Variant.types';
 export type { ViewportNames, BreakpointNames } from './Viewport.types';
+export type { OnAddToCartClick } from '~/contexts/AddToCartContext/AddToCartContext.types';
