de85268b56a6bb700295a0494935a041cfbddf67
Mustafa Mansour <33766083+MkMan@users.noreply.github.com>
2021-08-05 14:56:49 +1000

    feat(CON-465): migrate DialogBanner to TS

--- src/components/DialogBanner/DialogBanner.tsx
+++ src/components/DialogBanner/DialogBanner.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { useEscapeKeyListener, useOverflowHidden } from '~/customHooks';
 import { Button } from '~/components/Button';
-import { Heading } from '~/components/Heading/index.ts';
+import { Heading } from '~/components/Heading';
 import { Hyperlink } from '~/components/Hyperlink';
 import { Icon } from '~/components/Icon';
 import { P } from '~/components/Paragraph';
 import { Overlay } from '~/components/Overlay';
 import { Transition } from '~/components/Transition';
+import type { DialogBannerType } from './DialogBanner.types';
 import styles from './DialogBanner.module.css';
 
-const DialogBanner = ({
+const DialogBanner: DialogBannerType = ({
   className,
   copy,
   cta,
-  hasCloseButton,
-  heading,
-  isVisible,
+  hasCloseButton = true,
+  isVisible = false,
   message,
   onClose,
-  position,
-  theme,
+  position = 'top',
+  theme = 'dark',
+  heading,
 }) => {
   useEscapeKeyListener(onClose);
   useOverflowHidden(isVisible);
@@ -54,12 +54,7 @@ const DialogBanner = ({
             />
           </div>
           <div className={styles.contentContainer}>
-            <Heading
-              className={styles.heading}
-              level="1"
-              noTopMargin={true}
-              size="large"
-            >
+            <Heading className={styles.heading} level="1" size="large">
               {heading}
             </Heading>
             <P className={styles.message}>{message}</P>
@@ -70,7 +65,7 @@ const DialogBanner = ({
                 className={styles.closeButton}
                 isInline={true}
                 onClick={onClose}
-                title={copy.closeButtonTitle}
+                title={copy?.closeButtonTitle ?? 'Close Dialog Banner'}
               >
                 <Icon
                   className={styles.closeIcon}
@@ -85,9 +80,8 @@ const DialogBanner = ({
               <Hyperlink
                 className={styles.ctaButton}
                 dataTestRef={cta.dataTestRef}
+                hasTargetInNewWindow={cta.openInANewWindow}
                 id={cta.id}
-                isInline={true}
-                target={cta.openInANewWindow ? '_blank' : '_self'}
                 theme={'light'}
                 title={cta.title}
                 url={cta.url}
@@ -100,36 +94,6 @@ const DialogBanner = ({
       </Transition>
     </>
   );
-};
-
-DialogBanner.propTypes = {
-  className: PropTypes.string,
-  copy: PropTypes.shape({
-    closeButtonTitle: PropTypes.string,
-  }),
-  cta: PropTypes.object,
-  hasCloseButton: PropTypes.bool,
-  isVisible: PropTypes.bool,
-  message: PropTypes.string.isRequired,
-  onClose: PropTypes.func.isRequired,
-  position: PropTypes.oneOf(['top', 'bottom']),
-  theme: PropTypes.oneOf(['dark', 'black']),
-  heading: PropTypes.string.isRequired,
-};
-
-DialogBanner.defaultProps = {
-  className: undefined,
-  copy: {
-    closeButtonTitle: 'Close Dialog Banner',
-  },
-  cta: undefined,
-  hasCloseButton: true,
-  isVisible: false,
-  message: undefined,
-  onClose: undefined,
-  position: 'top',
-  theme: 'dark',
-  heading: undefined,
 };
 
 export { DialogBanner };
