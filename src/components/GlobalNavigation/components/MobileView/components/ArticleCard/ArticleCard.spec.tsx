369bd1c97a81ef89f96cff4c17c444dd42a3df43
Hassan Raza <m-h-r300@hotmail.com>
2021-09-01 11:11:24 +1000

    test(con-626): write ArticleCard unit tests

--- src/components/GlobalNavigation/components/MobileView/components/ArticleCard/ArticleCard.spec.tsx
+++ src/components/GlobalNavigation/components/MobileView/components/ArticleCard/ArticleCard.spec.tsx
@@ -1,8 +1,57 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import { ArticleCard } from './ArticleCard';
-// import { ArticleCardFixture } from './ArticleCard.fixture';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { ArticleCard } from './ArticleCard';
+import { ArticleCardFixture } from './ArticleCard.fixture';
 
-describe('<GlobalNavigation.MobileView.ArticleCard />', () => {
-  it.todo('should assert something');
+describe('<ArticleCard />', () => {
+  it('should render base component correctly', () => {
+    const { container } = render(
+      <ArticleCard
+        id={ArticleCardFixture.id}
+        label={ArticleCardFixture.label}
+        metaLabel={ArticleCardFixture.metaLabel}
+        title={ArticleCardFixture.title}
+        url={ArticleCardFixture.url}
+      />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should navigate to the article card by tab clicking', () => {
+    render(
+      <>
+        <ArticleCard
+          id={ArticleCardFixture.id}
+          label={ArticleCardFixture.label}
+          metaLabel={ArticleCardFixture.metaLabel}
+          title={ArticleCardFixture.title}
+          url={ArticleCardFixture.url}
+        />
+      </>,
+    );
+
+    const focusableElements = screen.getAllByRole('link');
+    const one = focusableElements[0];
+
+    expect(one).not.toHaveAttribute('tabIndex');
+  });
+
+  it('should not be able to be navigate to the article card by tab clicking', () => {
+    render(
+      <ArticleCard
+        id={ArticleCardFixture.id}
+        isVisible={false}
+        label={ArticleCardFixture.label2}
+        metaLabel={ArticleCardFixture.metaLabel}
+        title={ArticleCardFixture.title}
+        url={ArticleCardFixture.url}
+      />,
+    );
+
+    const focusableElements = screen.getAllByRole('link');
+    const one = focusableElements[0];
+
+    expect(one).toHaveAttribute('tabIndex', '-1');
+  });
 });
