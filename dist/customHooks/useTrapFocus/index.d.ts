eec0396bb2169eba00c13aea031b8f0c796fc04a
Matt <matt.nolan@commoncode.io>
2021-09-13 11:59:28 +1000

    style test

--- dist/customHooks/useTrapFocus/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-declare type UseTrapFocus = {
-    <Type = any>(shouldTrap?: boolean, query?: string): React.MutableRefObject<Type>[];
-};
-declare const useTrapFocus: UseTrapFocus;
-
-export { useTrapFocus };
--- dist/utils/portal/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare const getPortalRoot: (id: string) => HTMLElement | null;
-
-export { getPortalRoot };
