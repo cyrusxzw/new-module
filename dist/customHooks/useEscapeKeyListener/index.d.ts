70a614abb4eb35907e462f296557c0653c51f77f
Matt <matt.nolan@commoncode.io>
2021-09-16 10:40:07 +1000

    feat(con-721): googlemap theme

--- dist/customHooks/useEscapeKeyListener/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare type UseEscapeKeyListener = (onKeyPress: (event: KeyboardEvent) => void, shouldRunCallback?: boolean) => void;
-
-declare const useEscapeKeyListener: UseEscapeKeyListener;
-
-export { useEscapeKeyListener };
