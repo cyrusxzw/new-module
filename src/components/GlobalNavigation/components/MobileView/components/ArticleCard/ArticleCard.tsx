f9155261bfbf2c0d22753542d074b911fdbfc0b5
Max Lewis <maxlew@gmail.com>
2021-10-07 09:19:35 +1100

    fix(nav polish): use rem for all font sizes, remove article card scrolling

--- src/components/GlobalNavigation/components/MobileView/components/ArticleCard/ArticleCard.tsx
+++ src/components/GlobalNavigation/components/MobileView/components/ArticleCard/ArticleCard.tsx
@@ -24,7 +24,6 @@ const ArticleCard: ArticleCardType = ({
     compositionStyles.itemElement,
     compositionStyles.ornamentalWrapper,
     styles.leftAligned,
-    styles.mobileArticle,
   );
 
   return (
