f80efc0a9b9e0835985435d59ff5a606a6dee60d
Mustafa Mansour <33766083+MkMan@users.noreply.github.com>
2021-10-11 10:04:37 +1100

    refactor(Comp Types): use React's FC to fix Storybook docs (#583)
    * refactor: use React's FC to fix StoryBook docs
    
    * refactor(Common Types): provide an empty object by default

--- src/types/Component.types.ts
+++ src/types/Component.types.ts
@@ -1,15 +1,10 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import type { ReactElement, ReactNode } from 'react';
+import type { FC, ReactNode } from 'react';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type DefaultReactComponentReturn = ReactElement<any, any> | null;
-type PropsWithChildren<P> = P & { children?: ReactNode };
+type PropsWithChildren<Props> = Props & { children?: ReactNode };
+type PropsWithoutChildren<Props> = Props & { children?: never };
 
-type ComponentWithChildren<P = {}> = (
-  props: PropsWithChildren<P>,
-) => DefaultReactComponentReturn;
-type ComponentWithoutChildren<P = {}> = (
-  props: P,
-) => DefaultReactComponentReturn;
+type ComponentWithChildren<Props = {}> = FC<PropsWithChildren<Props>>;
+type ComponentWithoutChildren<Props = {}> = FC<PropsWithoutChildren<Props>>;
 
 export type { ComponentWithChildren, ComponentWithoutChildren };
