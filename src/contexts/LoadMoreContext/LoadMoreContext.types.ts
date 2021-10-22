071f4f79fc6979865facb55ee83c60d175ceb4c6
Mustafa Mansour <33766083+MkMan@users.noreply.github.com>
2021-10-22 15:10:05 +1100

    feat(LoadMoreButton): migrate to TypeScript (#604)

--- src/contexts/LoadMoreContext/LoadMoreContext.types.ts
+++ src/contexts/LoadMoreContext/LoadMoreContext.types.ts
@@ -4,7 +4,7 @@ import type { ComponentWithChildren } from '~/types';
 type LoadMoreContextProps = {
   /**
     A callback function that takes `sku<string>`, `LoadMoreDispatch<function>`, `ADD_TO_CART_ACTION_TYPES<array[string]>`
-    as arguments. See [LoadMoreButton.onClick.js mock](https://github.com/aesop/aesop-gel/tree/develop/src/components/LoadMoreButton/__mocks__/LoadMoreButton.onClick.js)
+    as arguments. See [LoadMoreButton.onClick.ts mock](https://github.com/aesop/aesop-gel/tree/develop/src/components/LoadMoreButton/__mocks__/LoadMoreButton.onClick.ts)
     for an example. ___Required___
    */
   onClick: MouseEventHandler<HTMLButtonElement>;
