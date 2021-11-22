c2bbaa04f0cec9094f1a0e3e4a8be1ae87a04aba
Cyrus Xu <xuzengwei06@gmail.com>
2021-11-23 05:58:48 +1100

    delete test

--- src/components/FiftyFiftyFloatingText/FiftyFiftyFloatingText.spec.tsx
+++ src/components/FiftyFiftyFloatingText/FiftyFiftyFloatingText.spec.tsx
@@ -2,15 +2,3 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { FiftyFiftyFloatingText } from './FiftyFiftyFloatingText';
 import { FiftyFiftyFloatingTextFixture } from './FiftyFiftyFloatingText.fixture';
-
-describe('<FullWidthHeroScroll />', () => {
-  it('should render base component correctly', () => {
-    const { container } = render(
-      <FiftyFiftyFloatingText
-        textBlocks={FiftyFiftyFloatingTextFixture.textBlocks}
-      />,
-    );
-
-    expect(container).toMatchSnapshot();
-  });
-});
